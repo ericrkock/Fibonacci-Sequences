4cbc90927c4780e35f4180f32af6f894923de8f6
Eric R. Kock <eric.r.kock@gmail.com>
2019-07-20 16:56:03 +0100

    JS updates

--- fibonacci-Javascript/fibonacci.js
+++ fibonacci-Javascript/fibonacci.js
@@ -1,30 +1,72 @@
-// Simple way
-// Recursion way
-// Reduced way
+// Calculate the Fibonacci Sequences in Javascript - 3 Ways
+// 1. Simple Way - Loop
+// 2. Recursion Way
+// 3. Reduced Way
+// ========================================================
 
-// Simple Way - Position
-fibonacciSimple(); // Start the simple way
-function fibonacciSimple() {
-    const fibonacci = [0,1];
+// 1. Simple Way
+//console.log(fibonacciLoop(101));
+function fibonacciLoop(index) {
     var fiboNew;
     var fiboPrev = 1;
     var fiboFirst = 0;
-    console.log("Pos:",1,"-", 0); // Show position 1
-    console.log("Pos:",2,"-", 1); // Show position 2
-    for (sequence = 3; sequence <= 102; sequence++) {
+    if (index < 2) return index;
+    for (let sequence = 2; sequence <= index; sequence++) {
         fiboNew = fiboFirst + fiboPrev;
-        fibonacci.push(fiboNew);
         fiboFirst = fiboPrev;
         fiboPrev = fiboNew;
-        console.log("Pos:",sequence,"-",fiboNew); // Show each remaining positions
     }
-    console.log(fibonacci); // Show Array
+    return fiboNew;
 }
 
-// Recursion Way - Index
-var fibonacciRecursion = function(index) {
+// 2.1 Recursion Way
+//console.log(fibonacciRecursion(10));
+function fibonacciRecursion(index) {
     if (index < 2) return index;
     return fibonacciRecursion(index-1) + fibonacciRecursion(index-2);
 };
-//console.log(fibonacciRecursion(4)); // Calculate and show the Fibonacci sequence on index
-//fibonacciRecursion(5); // Calculate the Fibonacci sequence on index
+
+// 2.2 Recursion Way with memory
+const fibonacci = (() => {
+    
+    const seq = [];
+    return (n, num) => {
+        if (n === num) return seq;
+        seq.length < 2 ? seq.push(n) : seq.push(seq[n - 2] + seq[n - 1]);
+    };
+})();
+
+const getSequence = (num, i = 0) => {
+    let r;
+    if (r = fibonacci(i, num)) return r;
+    return getSequence(num, ++i);
+}
+console.log(getSequence(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+
+
+
+
+// 3. Reduce function Way
+//console.log(fibonacciReduce(2));
+function fibonacciReduce(index) {
+    if (index < 2) return index;
+    return new Array(index).fill(1).reduce((arr, _ ,i) => {
+        arr.push((i <= 1) ? i : arr[i-2] + arr[i-1])
+        return arr
+    },[]) ;
+  }
+
+
+// Factorial
+//console.log(factorial(5));
+function factorial(n) {
+    if (n < 2) return n;
+    return n * factorial(n -1);
+}
+
+
+
+
+
+ 
