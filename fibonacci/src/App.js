2478a00d551fc2a5ea5e0e5be97abb1fb807c3b6
ericrkock <ericrkock@gmail.com>
2019-07-29 20:40:56 +0100

    Layout adjustments

--- fibonacci/src/App.js
+++ fibonacci/src/App.js
@@ -27,14 +27,23 @@ function App() {
             <fieldset>
               <legend> Give an index for each way </legend>
               <p className="form-label">1. SIMPLE LOOP WAY</p>
-                <input className="form-field" type="text" placeholder="give an index" />
+                <input className="form-field" type="text" placeholder="index ?" />
                 <button type="submit">Calculate</button>
+                <result id="resultLoopWay">Loop Way</result>
+                <button type="submit">Reset Index</button>
               <p className="form-label">2. RECURSION WAY</p>
-                <input className="form-field" type="text" placeholder="give an index" />
+                <input className="form-field" type="text" placeholder="index ?" />
                 <button type="submit">Calculate</button>
+                <result id="resultLoopWay">Recursion Way</result>
+                <button type="submit">Reset Index</button>
               <p className="form-label">3. REDUCE WAY</p>
-                <input className="form-field" type="text" placeholder="give an index" />
+                <input className="form-field" type="text" placeholder="index ?" />
                 <button type="submit">Calculate</button>
+                <result id="resultLoopWay">Reduce Way</result>
+                <button type="submit">Reset Index</button>
+              <p className="resetResults">
+                <button id="btn-reset" type="submit">Reset all</button>
+              </p>
             </fieldset>
           </form>
       </div> 
