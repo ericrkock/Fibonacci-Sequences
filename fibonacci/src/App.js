7c89e63b1dc5a2eb8722319e9c2ee94b6f5e8d1f
Eric R. Kock <eric.r.kock@gmail.com>
2019-08-05 14:45:20 +0100

    Updates

--- fibonacci/src/App.js
+++ fibonacci/src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 //mport logo from './logo.svg';
 import './App.css';
+import './calculations.js';
 
 function App() {
   return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
     <div className="App">
       <h2>3 Ways to calculate the Fibonacci Sequences</h2>
         <div>
@@ -28,7 +13,7 @@ function App() {
               <legend> Give an index for each way </legend>
               <p className="form-label">1. SIMPLE LOOP WAY</p>
                 <input className="form-field" type="text" placeholder="index ?" />
-                <button type="submit">Calculate</button>
+                <button onClick="fiboLoop()" class="loopWay" type="submit">Calculate</button>
                 <result id="resultLoopWay">Loop Way</result>
                 <button type="submit">Reset Index</button>
               <p className="form-label">2. RECURSION WAY</p>
