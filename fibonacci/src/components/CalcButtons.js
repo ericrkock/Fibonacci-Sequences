7f6e9ab27718ee34788408197154c46baa853276
ericrkock <ericrkock@gmail.com>
2019-08-18 11:17:39 +0100

    Sync

--- fibonacci/src/components/CalcButtons.js
+++ fibonacci/src/components/CalcButtons.js
@@ -2,16 +2,46 @@ import React from 'react';
 import Button from 'react-bootstrap/Button'; 
 //import FiboLoop from "./FiboLoop";
 
+class CalcButtons extends React.Component {
+   wayLoop(index) {
+      console.log("Loop Way Calculation", index);
+      var fiboNew;
+      var fiboPrev = 1;
+      var fiboFirst = 0;
+  
+      if (index <= 1 && index >= 0) return (index);
+      for (let sequence = 2; sequence <= index; sequence++) {
+          fiboNew = fiboFirst + fiboPrev;
+          fiboFirst = fiboPrev;
+          fiboPrev = fiboNew;
+      }
+      console.log("Calculation: ", fiboNew);
+      return fiboNew;
+      
+   }
 
-const CalcButtons = function() {
-   return (
-      <div className="buttoncard">
-         <Button variant="success">Loop Way</Button>
-         <Button variant="success">Recursion Way</Button>
-         <Button variant="success">Reduce Way</Button>
-         <Button variant="warning">Reset</Button>
-      </div>
-   );
+   wayRecursion() {
+      console.log("Recursion Way Calculation");
+   }
+
+   wayReduce() {
+      console.log("Reduce Way Calculation");
+   }
+
+   resetIndex() {
+      console.log("Reset Index Input");
+   }
+
+   render () {
+      return (
+         <div className="buttoncard">
+            <Button variant="success" onClick={this.wayLoop}>Loop Way</Button>
+            <Button variant="success" onClick={this.wayRecursion}>Recursion Way</Button>
+            <Button variant="success" onClick={this.wayReduce}>Reduce Way</Button>
+            <Button variant="warning" onClick={this.resetIndex}>Reset</Button>
+         </div>
+      );
+   }
 };
 
 export default CalcButtons;
--- fibonacci/src/components/FiboLoop.js
+++ fibonacci/src/components/FiboLoop.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
-
-const FiboLoop = function(index) {
-    var fiboNew;
-    var fiboPrev = 1;
-    var fiboFirst = 0;
+class FiboLoop extends React.Component {
+    FiboLoop(index) {
+        var fiboNew;
+        var fiboPrev = 1;
+        var fiboFirst = 0;
     
-    if (index <= 1 && index >= 0) return (index);
-    for (let sequence = 2; sequence <= index; sequence++) {
-        fiboNew = fiboFirst + fiboPrev;
-        fiboFirst = fiboPrev;
-        fiboPrev = fiboNew;
+        if (index <= 1 && index >= 0) return (index);
+        for (let sequence = 2; sequence <= index; sequence++) {
+            fiboNew = fiboFirst + fiboPrev;
+            fiboFirst = fiboPrev;
+            fiboPrev = fiboNew;
+        }
     }
 
-    console.log(fiboNew);
-    return (fiboNew);
-    
+    render () {
+        return (
+            <div>
+                console.log(fiboNew);
+                <div>fiboNew</div>
+            </div>
+        );
+    }
 }
 
 export default FiboLoop;
